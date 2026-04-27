0722130d156dee2a9921601c9388abc9f43c705d
saranyapaulraj15-dev <saranyapaulraj15@gmail.com>
2026-04-27 12:55:45 +0530

    Math project -by manager

--- src/learnGitHub/org/Maths.java
+++ src/learnGitHub/org/Maths.java
@@ -1,10 +1,20 @@
 package learnGitHub.org;
 
 public class Maths {
+	
+	//This project created by Saranya
+	
+	//1.Addition
+	//2.Substraction
+	//3.Multiplication
+	//4.Division
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) { //This is main method
+		
+		
+		
+		
 	}
 
 }
